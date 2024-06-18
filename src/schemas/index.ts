5a6084ab43cc52730071c164c6d8d096833d8adf
Lirae Que Data <liraedata59@gmail.com>
2024-06-18 17:48:54 +0800

    Add auth schema exports

---
+++ src/schemas/index.ts
@@ -0,0 +1,3 @@
+export { forgotPasswordSchema } from "@/schemas/auth/forgotPasswordSchema";
+export { loginSchema } from "@/schemas/auth/loginSchema";
+export { registerSchema } from "@/schemas/auth/registerSchema";
