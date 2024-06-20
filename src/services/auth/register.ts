5460e85fdad65f32d7add7023b7f809dab490053
Lirae Que Data <liraedata59@gmail.com>
2024-06-20 23:19:21 +0800

    Update RegisterService method in register.ts

--- src/services/auth/register.ts
+++ src/services/auth/register.ts
@@ -1,18 +1,19 @@
-const baseUrl = "http://localhost:8000/";
-
+import { axiosInstance } from "@/constants/axiosInstance";
 
 interface RegisterServiceProps {
-  username : string;
-  email : string;
-  password : string;
-  rePassword : string;
+  username: string;
+  email: string;
+  password: string;
+  rePassword: string;
+}
+
+const RegisterService = async (data: RegisterServiceProps) => {
+  try {
+    const response = await axiosInstance.post("auth/register/", data);
+    return response;
+  } catch (error) {
+    throw error;
+  }
 };
-
-const RegisterService = async (data : RegisterServiceProps) => {
-  const url = new URL("api/v1/auth/login", baseUrl);
-
-
-};
-
 
 export { RegisterService };
