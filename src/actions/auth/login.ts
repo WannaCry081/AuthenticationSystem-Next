39cbb211ef6ba61ba5ce76970f6fc1348535ffc3
Lirae Que Data <liraedata59@gmail.com>
2024-06-20 23:21:30 +0800

    Update login action in login.ts

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -1,9 +1,34 @@
 "use server";
 import * as z from "zod";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 import { LoginSchema } from "@/schemas";
+import { LoginService } from "@/services";
 
-const Login = async (values : z.infer<typeof LoginSchema>) => {
+const Login = async (values: z.infer<typeof LoginSchema>) => {
+  const cookieStore = cookies();
+  const validatedFields = LoginSchema.safeParse({
+    email: values.email,
+    password: values.password,
+  });
 
+  if (!validatedFields.success) {
+    return {
+      error: "Invalid Fields.",
+    };
+  }
+
+  const response = await LoginService(validatedFields.data);
+
+  if (!response) {
+    return {
+      error: "Invalid Credentials",
+    };
+  }
+
+  cookieStore.set("access", response.data.access);
+  cookieStore.set("refresh", response.data.refresh);
+  redirect("/");
 };
 
 export { Login };
