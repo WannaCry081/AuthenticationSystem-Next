a841091649749d5401cf3182c52e9ff9cece6775
Lirae Que Data <liraedata59@gmail.com>
2024-06-20 23:21:55 +0800

    Update register action in register.ts

--- src/actions/auth/register.ts
+++ src/actions/auth/register.ts
@@ -1,9 +1,36 @@
 "use server";
 import * as z from "zod";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 import { RegisterSchema } from "@/schemas";
+import { RegisterService } from "@/services";
 
-const Register = async (values : z.infer<typeof RegisterSchema>) => {
+const Register = async (values: z.infer<typeof RegisterSchema>) => {
+  const cookieStore = cookies();
+  const validatedFields = RegisterSchema.safeParse({
+    username: values.username,
+    email: values.email,
+    password: values.password,
+    rePassword: values.rePassword,
+  });
 
+  if (!validatedFields.success) {
+    return {
+      error: "Invalid Fields",
+    };
+  }
+
+  const response = await RegisterService(validatedFields.data);
+
+  if (!response) {
+    return {
+      error: "Invalid Credentials",
+    };
+  }
+
+  cookieStore.set("access", response.data.access);
+  cookieStore.set("refresh", response.data.refresh);
+  redirect("/");
 };
 
 export { Register };
