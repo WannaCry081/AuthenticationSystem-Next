1b47b826894dff34971404ab88a7c5df59ab3de6
Lirae Que Data <liraedata59@gmail.com>
2024-06-21 19:49:15 +0800

    Add cookieConfig method in config.ts

---
+++ src/constants/config.ts
@@ -0,0 +1,14 @@
+export function cookieConfig(maxAge: number) {
+  const ENVIRONMENT = process.env.NEXT_PUBLIC_ENV;
+  const HOST = process.env.NEXT_PUBLIC_HOSt;
+
+  const config = {
+    maxAge: maxAge,
+    path: "/",
+    domain: HOST,
+    httpOnly: !(ENVIRONMENT === "production"),
+    secure: ENVIRONMENT === "production",
+  };
+
+  return config;
+}
