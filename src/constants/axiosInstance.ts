28e1076c88d7716fe1cfb321e412cb669769b27d
Lirae Que Data <liraedata59@gmail.com>
2024-06-21 20:02:20 +0800

    Add axios instance

--- src/constants/axiosInstance.ts
+++ src/constants/axiosInstance.ts
@@ -1,15 +1,37 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL : "http://localhost:8000/"
+  baseURL : process.env.NEXT_PUBLIC_API_URL,
+  headers : {
+    "Content-Type" : "application/json"
+  },
 }); 
 
 axiosInstance.interceptors.request.use(
+  (request) => {
 
+    // add access token to the header 
+    return request;
+  }
 );
 
 axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
 
+    if (error.config && error.response){
+      if (error.response.status === 401) {
+        // refresh token 
+
+      } else {
+        // restart the request
+      }
+
+      if (error.response.status === 403) {
+        // Handle error
+      }
+    }
+  }
 );
 
 
