baf5a5a7c4a2d62043ee38604c3b139522b41f36
Lirae Que Data <liraedata59@gmail.com>
2024-06-21 14:46:59 +0800

    Update ui in login/page.tsx

--- src/components/auth/login/page.tsx
+++ src/components/auth/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import * as z from "zod";
 import Link from "next/link";
+import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -19,102 +21,115 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { PasswordInput } from "@/components/ui/password-input";
-import { loginSchema as formSchema } from "@/schemas";
+import { LoginSchema } from "@/schemas";
+import { Login } from "@/actions";
+import { FormError } from "@/components/ui/form-error";
 
 export default function LoginView() {
-  const form = useForm({
-    resolver: zodResolver(formSchema),
+  const [error, setError] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  const onSubmit = (values: any) => {
-    alert(values.password);
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    setError("");
+    startTransition(() => {
+      Login(values).then((data: any) => {
+        if (data) {
+          setError(data.error);
+        }
+      });
+    });
   };
 
   return (
-    <section className="fixed py-4 sm:p-6 md:p-8 sm:relative sm:top-4 bg-inherit mx-auto w-full">
-      <Card className="mx-auto max-w-lg bg-neutral-900 border-0 sm:border border-neutral-700 text-neutral-300 font-mono">
-        <CardHeader>
-          <h1 className="text-2xl sm:text-5xl text-neutral-100">Login.</h1>
-          <p className="pt-2 text-sm sm:text-base font-medium">
-            Welcome back! Sign in with your verified email and password to
-            access your account.
-          </p>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="uppercase text-xs font-bold text-neutral-500">
-                      Email Address
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        minLength={5}
-                        maxLength={101}
-                        className="bg-neutral-700 border-0"
-                        placeholder="Enter your email address"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+    <Card className="mx-auto max-w-lg bg-neutral-900 border-0 sm:border border-neutral-700 text-neutral-300 font-mono">
+      <CardHeader>
+        <h1 className="text-4xl sm:text-5xl font-semibold  text-neutral-100">Login.</h1>
+        <p className="pt-2 text-sm sm:text-base font-medium">
+          Welcome back! Sign in with your verified email and password to access
+          your account.
+        </p>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="uppercase text-xs sm:text-sm font-semibold text-neutral-500">
+                    Email Address
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      minLength={5}
+                      maxLength={101}
+                      className="bg-neutral-700 border-0 text-base sm:h-12"
+                      placeholder="Enter your email address"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="uppercase text-xs font-bold text-neutral-500">
-                      Password
-                    </FormLabel>
-                    <FormControl>
-                      <PasswordInput
-                        className="bg-neutral-700 border-0"
-                        placeholder="Enter your password"
-                        field={field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="uppercase text-xs sm:text-sm font-semibold text-neutral-500">
+                    Password
+                  </FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      disabled={isPending}
+                      className="bg-neutral-700 border-0 text-base sm:h-12"
+                      placeholder="Enter your password"
+                      field={field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-              <Link
-                href="/forgot-password"
-                className="float-right text-sm sm:text-base text-neutral-500 font-medium hover:text-neutral-300"
-              >
-                Forgot Password?
-              </Link>
-
-              <Button
-                type="submit"
-                size="lg"
-                className="bg-neutral-700 w-full hover:bg-neutral-800"
-              >
-                Login
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-        <CardFooter>
-          <Link
-            href="/register"
-            className="text-sm sm:text-base text-neutral-500 text-center w-full font-medium hover:text-neutral-300"
-          >
-            Don't have an account?
-          </Link>
-        </CardFooter>
-      </Card>
-    </section>
+            <FormError message={error} />
+            <Link
+              href="/forgot-password"
+              className="float-right text-sm sm:text-base text-neutral-500 font-medium hover:text-neutral-300"
+            >
+              Forgot Password?
+            </Link>
+            <Button
+              disabled={isPending}
+              type="submit"
+              size="lg"
+              className="bg-neutral-950 w-full hover:bg-neutral-800"
+            >
+              Login
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter>
+        <Link
+          href="/register"
+          className="text-sm sm:text-base text-neutral-500 text-center w-full font-medium hover:text-neutral-300"
+        >
+          Don't have an account?
+        </Link>
+      </CardFooter>
+    </Card>
   );
 }
