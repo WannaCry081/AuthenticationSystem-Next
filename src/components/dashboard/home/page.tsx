e76cddaf25764cce95f2489c3d5844ef21049ce9
Lirae Que Data <liraedata59@gmail.com>
2024-06-22 17:09:00 +0800

    Update dashboard ui

--- src/components/dashboard/home/page.tsx
+++ src/components/dashboard/home/page.tsx
@@ -1,8 +1,49 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Logout } from "@/actions";
+
 const HomeView = () => {
+
+  const handleLogout = async () => {
+    try {
+      await Logout();
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  };
+
   return (
-    <section>
-      <h1>Home Page</h1>
-    </section>
+    <Card className="bg-inherit border-0 border-neutral-700  mx-auto text-neutral-100 font-mono max-w-lg sm:border mt-4">
+      <CardHeader>
+        <h1 className="text-2xl md:text-4xl font-semibold">Profile.</h1>
+      </CardHeader>
+      <CardContent>
+        <div>
+          
+          <Button
+            onClick={handleLogout}
+            size="lg"
+            variant="destructive"
+            className="bg-destructive/60 w-full"
+          >
+            Logout
+          </Button>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className="w-full text-center">
+          <p className="text-xs uppercase text-neutral-400">
+            ©2024 by WannaCry081
+          </p>
+        </div>
+      </CardFooter>
+    </Card>
   );
 };
 
