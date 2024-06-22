d5257924009c1153c3567c89fbd6887d59db6ce4
Lirae Que Data <liraedata59@gmail.com>
2024-06-22 17:06:40 +0800

    Add logout service in services/index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export { ForgotPasswordService } from "./auth/forgot-password";
 export { LoginService } from "./auth/login";
 export { RegisterService } from "./auth/register";
+export { LogoutService } from "./auth/logout";
