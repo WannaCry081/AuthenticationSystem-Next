bf6bed0337968d4f2769681cfb568e78697b5407
Lirae Que Data <liraedata59@gmail.com>
2024-06-22 17:06:22 +0800

    Add logout actions in actions/index.ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
 export { ForgotPassword } from "@/actions/auth/forgot-password";
 export { Login } from "@/actions/auth/login";
 export { Register } from "@/actions/auth/register";
+export { Logout } from "@/actions/auth/logout";
