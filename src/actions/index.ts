da20e8326f3e62a051899e1a667902e4de485971
Lirae Que Data <liraedata59@gmail.com>
2024-06-24 23:05:42 +0800

    Add reset password action export

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,3 +2,4 @@ export { ForgotPassword } from "@/actions/auth/forgot-password";
 export { Login } from "@/actions/auth/login";
 export { Register } from "@/actions/auth/register";
 export { Logout } from "@/actions/auth/logout";
+export { ResetPassword } from "@/actions/auth/reset-password";
