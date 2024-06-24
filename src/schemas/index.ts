20151966635a96599ef8d35b37409af5253c2061
Lirae Que Data <liraedata59@gmail.com>
2024-06-25 00:01:38 +0800

    Update schema component exports

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,4 +1,4 @@
-export { ForgotPasswordSchema } from "@/schemas/auth/forgot-password";
 export { LoginSchema } from "@/schemas/auth/login";
 export { RegisterSchema } from "@/schemas/auth/register";
+export { VerifySchema } from "@/schemas/auth/verify";
 export { ResetPasswordSchema } from "@/schemas/auth/reset-password";
