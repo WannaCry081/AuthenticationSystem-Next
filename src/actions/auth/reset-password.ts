a2ef284a9829ba875b007e13a960cefd1b449c15
Lirae Que Data <liraedata59@gmail.com>
2024-06-24 23:04:49 +0800

    Create actions/auth/reset-password.ts

---
+++ src/actions/auth/reset-password.ts
@@ -0,0 +1,31 @@
+"use server";
+import * as z from "zod";
+import { redirect } from "next/navigation";
+import { ResetPasswordSchema } from "@/schemas";
+import { ResetPasswordService } from "@/services";
+
+const ResetPassword = async (values: z.infer<typeof ResetPasswordSchema>) => {
+  const validatedFields = ResetPasswordSchema.safeParse({
+    email: values.email,
+    resetCode: values.resetCode,
+    newPassword: values.newPassword,
+  });
+
+  if (!validatedFields.success) {
+    return {
+      error: "Invalid Fields",
+    };
+  }
+
+  const response = await ResetPasswordService(validatedFields.data);
+
+  if (!response) {
+    return {
+      error: "Invalid Credentials",
+    };
+  }
+
+  redirect("/login");
+};
+
+export { ResetPassword };
