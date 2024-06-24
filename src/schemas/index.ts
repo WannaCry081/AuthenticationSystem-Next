29edeb696be36b2592f004260003c5fa6e471574
Lirae Que Data <liraedata59@gmail.com>
2024-06-24 23:16:43 +0800

    Add reset password schema export

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
 export { ForgotPasswordSchema } from "@/schemas/auth/forgot-password";
 export { LoginSchema } from "@/schemas/auth/login";
 export { RegisterSchema } from "@/schemas/auth/register";
+export { ResetPasswordSchema } from "@/schemas/auth/reset-password";
