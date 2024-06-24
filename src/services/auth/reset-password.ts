6a3f2c40cb5c54b4976cea38a6c77cf087b88cf1
Lirae Que Data <liraedata59@gmail.com>
2024-06-24 23:56:08 +0800

    Rename forgot-password.ts to reset-password.ts

--- src/services/auth/reset-password.ts
+++ src/services/auth/reset-password.ts
@@ -2,20 +2,15 @@ import { axiosInstance } from "@/constants/axiosInstance";
 
 interface ResetPasswordServiceProps {
   email: string;
-  resetCode: string;
-  newPassword: string;
 }
 
 const ResetPasswordService = async (data: ResetPasswordServiceProps) => {
   try {
-    const response = await axiosInstance.post(
-      "auth/reset-password/verify/",
-      data
-    );
+    const response = await axiosInstance.post("auth/reset-password/", data);
     return response;
   } catch (error) {
     throw error;
   }
 };
 
-export { ResetPasswordService };
+export default ResetPasswordService;
