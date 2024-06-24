ec4f535dd67cdad308214362e6d56c425dda9030
Lirae Que Data <liraedata59@gmail.com>
2024-06-24 23:18:22 +0800

    Add reset password service export

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,4 @@ export { ForgotPasswordService } from "./auth/forgot-password";
 export { LoginService } from "./auth/login";
 export { RegisterService } from "./auth/register";
 export { LogoutService } from "./auth/logout";
+export { ResetPasswordService } from "./auth/reset-password";
