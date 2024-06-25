4895a21db61d2c425b6253e866a4ad94dd9674a2
Lirae Que Data <liraedata59@gmail.com>
2024-06-25 23:11:45 +0800

    Add get profile service export in index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,5 @@ export { default as RegisterService } from "./auth/register";
 export { default as LogoutService } from "./auth/logout";
 export { default as VerifyService } from "./auth/verify";
 export { default as ResetPasswordService } from "./auth/reset-password";
+
+export { default as GetProfileService } from "./users/get-profile";
