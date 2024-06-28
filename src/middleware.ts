8b6964aef4d7039b178cb2fddd75d479d7ac6598
Lirae Que Data <liraedata59@gmail.com>
2024-06-29 01:30:08 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,20 @@
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export default async function middleware(request : NextRequest){
-  return;
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  const origin = url.origin;
+  const pathName = url.pathname;
+  const access = request.cookies.get("access")?.value;
+  const refresh = request.cookies.get("refresh")?.value;
+
+  if (access || refresh) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(`${origin}/login`);
 }
+
+export const config = {
+  matcher: ["/home"],
+};
